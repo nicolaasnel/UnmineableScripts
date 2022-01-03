04359fa36ce0a57917827515002631e68dd707d6
Nicolaas Nel <nicolaasnel@users.noreply.github.com>
2022-01-03 10:23:03 +0200

    Make worker name a parameter

--- kawpow_shib.bat
+++ kawpow_shib.bat
@@ -1,4 +1,4 @@
 if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
 start msedge http://127.0.0.1:22333 https://unmineable.com/coins/SHIB/address/%shib_wallet%
-call "%nbminer_executable%" -a kawpow -o stratum+tcp://kp.unmineable.com:3333 -u  SHIB:%shib_wallet%.RTX3070#kmxp-e69q -log
+call "%nbminer_executable%" -a kawpow -o stratum+tcp://kp.unmineable.com:3333 -u  SHIB:%shib_wallet%.%worker_name%#kmxp-e69q -log
 pause
