212b243bdb03241d95ff5adb46b5fdf4f30178cd
Nicolaas Nel <nicolaasnel@users.noreply.github.com>
2022-01-26 11:55:49 +0200

    Update miner.bat

--- scripts/miner.bat
+++ scripts/miner.bat
@@ -5,5 +5,5 @@ call set wallet=%%%tmp%%%
 if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
 start msedge http://127.0.0.1:22333 https://unmineable.com/coins/%coin%/address/%wallet%
 if "%algo%"=="kawpow" (set route=kp) else (set route=%algo%)
-call "%nbminer_executable%" -a %algo% -o stratum+tcp://%route%.unmineable.com:3333 -u %coin%:%wallet%.%worker_name%#89lx-o3c3 -log %nbminer_options%
+call "%nbminer_executable%" -a %algo% -o stratum+tcp://%route%.unmineable.com:3333 -u %coin%:%wallet%.%worker_name%#fesn-6xwf -log %nbminer_options%
 pause
